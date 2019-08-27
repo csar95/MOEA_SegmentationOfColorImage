916f2ca17af8550cc973da35f3fd529b5feed70b
Cesar Gutierrez Carrero <cesarguti47@gmail.com>
2019-08-27 16:48:40 +0200

    Fitness assignment and environmental selection

---
+++ src/models/Solution.java
@@ -0,0 +1,75 @@
+package models;
+
+public class Solution {
+	
+	private int[] solution;
+	private double overallDeviation;
+	private double edgeValue;
+	
+	private int strength; // Number of solutions that a given solution dominates.
+	private int rawFitness; // Sum of the strength values of the solutions that dominate a given solution.
+	private double density;
+	private double fitness;
+	
+	public Solution(int[] solution, double overallDev, double edgeVal) {
+		this.setSolution(solution);
+		this.setOverallDeviation(overallDev);
+		this.setEdgeValue(edgeVal);
+	}
+	
+	public boolean dominates (Solution sol) {
+		if (this.overallDeviation < sol.getOverallDeviation() && this.edgeValue < sol.getEdgeValue()) return true;
+		return false;
+	}
+	
+	public double calculate_euclidean_distance_between_objectives (Solution sol) {				
+		return Math.sqrt(
+				Math.pow((this.overallDeviation - sol.getOverallDeviation()), 2) +
+				Math.pow((this.edgeValue - sol.getEdgeValue()), 2));		
+	}
+
+	public int[] getSolution() {
+		return this.solution;
+	}
+	public double getOverallDeviation() {
+		return this.overallDeviation;
+	}
+	public double getEdgeValue() {
+		return this.edgeValue;
+	}
+	public int getStrength() {
+		return this.strength;
+	}
+	public int getRawFitness() {
+		return rawFitness;
+	}
+	public double getDensity() {
+		return density;
+	}
+	public double getFitness() {
+		return fitness;
+	}
+	
+	public void setOverallDeviation(double overallDeviation) {
+		this.overallDeviation = overallDeviation;
+	}
+	public void setEdgeValue(double edgeValue) {
+		this.edgeValue = edgeValue;
+	}
+	public void setSolution(int[] solution) {
+		this.solution = solution;
+	}
+	public void setStrength(int strength) {
+		this.strength = strength;
+	}
+	public void setRawFitness(int rawFitness) {
+		this.rawFitness = rawFitness;
+	}
+	public void setDensity(double density) {
+		this.density = density;
+	}
+	public void setFitness(double fitness) {
+		this.fitness = fitness;
+	}
+	
+}
--- src/moea/MOEA.java
+++ src/moea/MOEA.java
@@ -2,11 +2,16 @@ package moea;
 
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Queue;
 
 import models.Pixel;
+import models.Solution;
 
 public class MOEA {
 	
@@ -14,34 +19,59 @@ public class MOEA {
 	private BufferedImage image;
 	private int height;
 	private int width;
-	private final int POPULATION_SIZE = 50;
+	private final int POPULATION_SIZE = 30;
+	private int keySol;
+	private HashMap<Integer, Solution> population;	
+	private final int ARCHIVE_SIZE = 8;
+	private HashMap<Integer, Solution> archive;
 	
 	public MOEA (int[] mst, BufferedImage img) {
+		this.keySol = 1;
 		this.image = img;
 		this.height = img.getHeight();
 		this.width = img.getWidth();
 		this.mst = mst;
+		this.population = new HashMap<Integer, Solution>();
+		this.archive = new HashMap<Integer, Solution>();
 	}
 	
 	public void run_algorithm () {
 		
-		List<int[]> population = new ArrayList<int[]>();
+		HashMap<Integer, Solution> union = new HashMap<Integer, Solution>();
+		
 		long start = System.currentTimeMillis(), finish;
-		// Generate an initial population
-		for (int i = 1; i <= this.POPULATION_SIZE; i++) {
-			int[] initialSolution = this.get_initial_solution(i);
-			List<ArrayList<Integer>> clusters = get_clusters(initialSolution);			
-			population.add(initialSolution);
-			
-			System.out.println("Overall deviation of " + i + "th solution: " + this.calculate_overall_deviation(clusters));
-			System.out.println("Edge value of " + i + "th solution: " + this.calculate_edge_value(clusters, initialSolution));
-			
-			finish = System.currentTimeMillis();
-			System.out.println(i + "th solution generated in " + (finish - start) / 1000. + " seconds.");
-			System.out.println("------------------------------------");
-			start = System.currentTimeMillis();
+		
+		// 1. Initialization
+		while (this.keySol <= this.POPULATION_SIZE) {
+			int[] initialSolution = this.get_initial_solution(this.keySol);
+			List<ArrayList<Integer>> clusters = get_clusters(initialSolution);
+			// Calculate objectives
+			this.population.put(this.keySol, new Solution(initialSolution, this.calculate_overall_deviation(clusters), this.calculate_edge_value(clusters, initialSolution)));
+			this.keySol++;
 		}
 		
+		// 50 pop - 3 mins | 30 pop - <1,5 min
+		finish = System.currentTimeMillis();
+		System.out.println("Initial solutions generated in " + (finish - start) / 1000. + " seconds.");
+		System.out.println("------------------------------------");
+		start = System.currentTimeMillis();
+		
+		union.clear();
+		union.putAll(this.population);
+		union.putAll(this.archive);
+		
+		// 2. Fitness assignment
+		calculate_strength_and_density(union);
+		calculate_fitness(union);
+		
+		// 3. Environmental selection
+		get_non_dominated(union);		
+		if (this.archive.size() < this.ARCHIVE_SIZE) {
+			populate_archive_with_remaining_best(union);
+		}
+		else if (this.archive.size() > this.ARCHIVE_SIZE) {
+			remove_most_similar_from_archive();
+		}
 	}
 	
 	private int[] get_initial_solution (int ithIndividual) {
@@ -141,7 +171,6 @@ public class MOEA {
 		double edgeValue = .0;
 		int pixelPos;
 		Pixel pixel;
-//		ArrayList<Integer> pixelCluster = null;
 		
 		// For every pixel in the image
 		for (int i = 0; i < this.height; i++) {
@@ -149,14 +178,6 @@ public class MOEA {
 				
 				pixel = new Pixel(this.image.getRGB(j, i));
 				pixelPos = i * this.width + j;
-				
-				// Get the cluster the pixel belongs to
-//				for (ArrayList<Integer> cluster : clusters) {
-//					if (cluster.contains(pixelPos)) {
-//						pixelCluster = cluster;
-//						break;
-//					}
-//				}
 				
 				// Add the euclidean distance to the edge value if neighbor pixel belongs to the same cluster
 				if (i > 0 && this.from_same_cluster(pixelPos, pixelPos - this.width, solution)) { // Upper pixel
@@ -203,5 +224,168 @@ public class MOEA {
 		if (solution[aux2] == rootPos) return true;
 		
 		return false;		
+	}
+	
+	/*
+	 * Calculates the strength of a solution based on the number of solutions that a given solution dominates.
+	 * Estimates the density of an area of the Pareto front. Density is calculated between the solution and its kth nearest neighbor in the population.
+	 */	
+	private void calculate_strength_and_density (HashMap<Integer, Solution> union) {
+		
+		ArrayList<Double> distancesToOtherSolutions = new ArrayList<Double>();
+		int strength, solKey, kthNearestNeighbor;
+		Solution solution, auxSolution;
+		double density;
+		
+		for (Map.Entry<Integer, Solution> sol : union.entrySet()) {
+			
+			strength = 0;
+			distancesToOtherSolutions.clear();
+			solKey = sol.getKey();
+			solution = sol.getValue();
+			
+			for (Map.Entry<Integer, Solution> auxSol : union.entrySet()) {				
+				if (solKey == auxSol.getKey()) continue;
+				auxSolution = auxSol.getValue();
+				if (solution.dominates(auxSolution)) strength++;
+				distancesToOtherSolutions.add(solution.calculate_euclidean_distance_between_objectives(auxSolution));				
+			}
+			
+			Collections.sort(distancesToOtherSolutions);
+			
+			kthNearestNeighbor = (int) Math.sqrt(union.size());
+			density = 1.0 / (distancesToOtherSolutions.get(kthNearestNeighbor) + 2.0);
+			
+			solution.setDensity(density);
+			solution.setStrength(strength);			
+			union.put(solKey, solution);			
+		}
+	}
+	
+	// Calculates the raw fitness as the sum of the strength values of the solutions that dominate a given candidate.
+	private void calculate_fitness (HashMap<Integer, Solution> union) {
+		
+		int rawFitness, solKey;
+		Solution solution, auxSolution;
+		
+		for(Map.Entry<Integer, Solution> sol : union.entrySet()) {
+			
+			rawFitness = 0;
+			solKey = sol.getKey();
+			solution = sol.getValue();
+			
+			for(Map.Entry<Integer, Solution> auxSol : union.entrySet()) {
+				
+				if (solKey == auxSol.getKey()) continue;
+				auxSolution = auxSol.getValue();
+				if (auxSolution.dominates(solution)) rawFitness += auxSolution.getStrength();
+			}
+			
+			solution.setRawFitness(rawFitness);
+			solution.setFitness(rawFitness + solution.getDensity());
+			union.put(sol.getKey(), solution);
+			
+			if (this.population.containsKey(solKey)) this.population.put(solKey, solution);
+			if (this.archive.containsKey(solKey)) this.archive.put(solKey, solution);			
+		}		
+	}
+
+	private boolean is_non_dominated_solution (int memberKey, HashMap<Integer, Solution> map) {
+		
+		Solution solution = map.get(memberKey), auxSolution;
+		
+		for(Map.Entry<Integer, Solution> sol : map.entrySet()) {
+			if (memberKey == sol.getKey()) continue;
+			auxSolution = sol.getValue();
+			if (auxSolution.dominates(solution)) return false;
+		}
+		
+		return true;
+	}
+	
+	private void get_non_dominated (HashMap<Integer, Solution> union) {
+		
+		this.archive.clear();
+		ArrayList<Integer> membersPendingToBeChecked = new ArrayList<Integer>(union.keySet());
+		
+		while (!membersPendingToBeChecked.isEmpty()) {
+			
+			int randomMemberKey = membersPendingToBeChecked.remove( (int) (Math.random() * membersPendingToBeChecked.size()) );
+			if (this.is_non_dominated_solution(randomMemberKey, union))
+				this.archive.put(randomMemberKey, union.get(randomMemberKey));
+			
+		}		
+	}
+	
+	private void populate_archive_with_remaining_best (HashMap<Integer, Solution> union) {
+		
+		double minFitness, solutionFitness;
+		int newArchiveMember = 0;
+		Solution solution;
+		ArrayList<Integer> membersInArchive = new ArrayList<Integer>(this.archive.keySet());
+		
+		for (int key : membersInArchive) {
+			union.remove(key);
+		}		
+		
+		while (this.archive.size() < this.ARCHIVE_SIZE) {
+			
+			minFitness = 999999999;
+			
+			for(Map.Entry<Integer, Solution> sol : union.entrySet()) {
+				
+				solution = sol.getValue();
+				solutionFitness = solution.getFitness();
+				if (solutionFitness < minFitness) {
+					minFitness = solutionFitness;
+					newArchiveMember = sol.getKey();
+				}
+			}
+			
+			this.archive.put(newArchiveMember, union.remove(newArchiveMember));
+		}
+		
+	}
+	
+	private void remove_most_similar_from_archive () {
+		
+		int solKey, kthNearestNeighbor, archiveKeyToDelete;
+		Solution solution, auxSolution;
+		ArrayList<Double[]> arrayForComparison = new ArrayList<Double[]>();
+		ArrayList<Double> distancesToOtherMembersOfTheArchive = new ArrayList<Double>();		
+		
+		while (this.archive.size() > this.ARCHIVE_SIZE) {
+			
+			arrayForComparison.clear();
+			
+			for (Map.Entry<Integer, Solution> sol : this.archive.entrySet()) {	
+				
+				distancesToOtherMembersOfTheArchive.clear();
+				solKey = sol.getKey();
+				solution = sol.getValue();
+				
+				for (Map.Entry<Integer, Solution> auxSol : this.archive.entrySet()) {				
+					if (solKey == auxSol.getKey()) continue;
+					auxSolution = auxSol.getValue();
+					distancesToOtherMembersOfTheArchive.add(solution.calculate_euclidean_distance_between_objectives(auxSolution));				
+				}
+				
+				Collections.sort(distancesToOtherMembersOfTheArchive);			
+				kthNearestNeighbor = (int) Math.sqrt(this.archive.size());			
+				arrayForComparison.add(new Double[] {(double) solKey, distancesToOtherMembersOfTheArchive.get(kthNearestNeighbor)});
+				
+			}
+			
+			// Order array by the euclidean distance of the kth nearest neighbor
+			Collections.sort(arrayForComparison,new Comparator<Double[]>() {
+	            public int compare(Double[] doubles, Double[] otherdDoubles) {
+	                return doubles[1].compareTo(otherdDoubles[1]);
+	            }
+	        });			
+			
+			archiveKeyToDelete = (int) ((double) arrayForComparison.get(0)[0]);
+			this.archive.remove(archiveKeyToDelete);
+		}
+		
 	}
 }
